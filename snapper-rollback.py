fade407d624c8c43d289d45f70587de9aeb008eb
Brian Morison <777373+bkmo@users.noreply.github.com>
2024-06-27 11:13:54 -0600

    Update snapper-rollback.py

--- snapper-rollback.py
+++ snapper-rollback.py
@@ -127,9 +127,9 @@ def rollback(subvol_main, subvol_main_newname, subvol_rollback_src, dev, dry_run
 def rollback_efi_partiton(efi_backup_dir,dry_run):
     full_path = efi_backup_dir+"/efi/EFI/Linux/"
     if dry_run:
-        LOG.info("rsync -avzq --delete {}* /efi/EFI/Linux/".format(full_path))
+        LOG.info("rsync -aq --delete {} /efi/EFI/Linux/".format(full_path))
     else:
-        os.system("rsync -avzq --delete {}* /efi/EFI/Linux/".format(full_path))
+        os.system("rsync -aq --delete {} /efi/EFI/Linux/".format(full_path))
 
 def main():
     args = parse_args()
